85df99f9cdc543d77db287bdca60efc933e7a27e
ericcnguyen04 <ericc.win@gmail.com>
2023-03-22 17:58:57 -0400

    yes

---
+++ script.js
@@ -0,0 +1,29 @@
+const x = 3
+const y = '7'
+const z = true
+
+if (x && y === 8) {
+    console.log('you win')
+} else {
+    console.log('you suck')
+}
+
+x++
+x = x + 1
+console.log(x)
+
+
+const myArr = [
+    'eren', 
+    'yeager',
+    'armin'
+]
+console.log(myArr[0]) // eren
+
+const myObject = {
+    model: "honda",
+    make: 'something',
+    year: 834
+}
+
+console.log(myObject.model) // honda
